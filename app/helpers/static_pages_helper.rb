10c09bf1f9155256765f437a60794798fd8fca68
yuki-2000 <88224293+yuki-2000@users.noreply.github.com>
2021-10-01 22:27:47 +0900

    use narou api

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -8,5 +8,22 @@ module StaticPagesHelper
         return a
       end
 
+      def index2()
+        url = "https://qiita.com/api/v2/items"
+        header = { Authorization: "Bearer xxxxx" } # 例) ヘッダーに"Bearer xxxxx"を付与
+        query = { page: 1, per_page: 20 }          # 例) 1ページ目、1ページごとのデータ取得数を20件にするquery       
+        client = HTTPClient.new
+        response = client.get(url, header: header, query: query) #headerとqueryを指定
+        JSON.parse(response.body)
+      end
+
+      def index3()
+        url = "https://api.syosetu.com/rank/rankget/?out=json&rtype=20210901-d&lim=20"
+        #header = { Authorization: "Bearer xxxxx" } # 例) ヘッダーに"Bearer xxxxx"を付与
+        #query = { out: json, rtype: 20211001-d , lim: 20 }          # 例) 1ページ目、1ページごとのデータ取得数を20件にするquery       
+        client = HTTPClient.new
+        response = client.get(url)
+        JSON.parse(response.body)
+      end
 
 end
