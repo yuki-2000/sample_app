9c6c31820409bfb22431e2e86eac28b92f668487
yuki-2000 <88224293+yuki-2000@users.noreply.github.com>
2021-10-01 22:03:45 +0900

    外部apiを使用
    https://qiita.com/shungo_m/items/0a56610d944e4458dedd

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 #ruby '3.0.2'
 
-
-
+#https://qiita.com/shungo_m/items/0a56610d944e4458dedd
+gem 'httpclient'
 
 gem 'bootstrap-sass', '3.3.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+
+    def index()
+        url = "https://qiita.com/api/v2/items"  # URLを設定
+        client = HTTPClient.new                 # インスタンスを生成
+        response = client.get(url)              # Getリクエスト
+        a = JSON.parse(response.body)  # 結果をjsonにパースして表示
+        return a
+      end
+
+
 end
